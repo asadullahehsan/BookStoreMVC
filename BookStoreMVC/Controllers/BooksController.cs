c7ad62c56bfec053ed309a79af212f3287efbdd1
Asadullah Ehsan <asadullahehsan10@gmail.com>
2025-05-21 18:10:05 +0500

    Add project files.

---
+++ BookStoreMVC/Controllers/BooksController.cs
@@ -0,0 +1,26 @@
+﻿using BookStoreMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStoreMVC.Controllers;
+
+public class BooksController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Details()
+    {
+        Book book = new Book()
+        {
+            Id = 1,
+            Title = "Learning ASP.NET Core 8.0",
+            Genre = "Programming & Software Development",
+            Price = 45,
+            PublishDate = new System.DateTime(2025, 05, 21),
+            Authors = new List<string> { "Jason De Oliveira", "Michel Bruchet" }
+        };
+        return View(book);
+    }
+}
